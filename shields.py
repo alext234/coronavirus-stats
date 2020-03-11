fbc36c8fdf3ef2b1738eba17cc68cf418cdd4124
Your Name <you@example.com>
2020-03-11 12:40:38 +0000

    misc bug fixes

--- shields.py
+++ shields.py
@@ -1,7 +1,7 @@
 import json
 def write_shields_json(filename, label='', message=''):    
     json_payload = {'schemaVersion': 1, 
-              'label': f'Top 10', 
+              'label': label, 
               'message': message}
     with open(filename, 'w') as fo:
         fo.write(json.dumps(json_payload))
